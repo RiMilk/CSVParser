1a5a690a84f0a62e8c82cecd7f740bc1e6a24d4e
RiMilk <rpakreeva@yandex.ru>
2023-01-29 23:17:10 +0300

    add parse data

--- parser.cpp
+++ parser.cpp
@@ -4,9 +4,10 @@ void reader(std::string fileName)
 {
     std::ifstream file(fileName);
     std::vector<std::string> header;
-    std::vector<std::map<std::string, std::string>> map;
+    std::map<std::string, std::map<std::string, std::string>> map;
 
     header = getHeader(fileName);
+    map = getData(fileName, header);
 }
 
 void output()
@@ -23,7 +24,7 @@ std::vector<std::string> getHeader(std::string fileName)
     std::string line = "";
     std::vector<std::string> header;
 
-    getline(file, line);
+    getline(file, line); //
 
     pos = line.find(',');
     line = line.erase(0, 1);
@@ -40,4 +41,55 @@ std::vector<std::string> getHeader(std::string fileName)
 
     file.close();
     return (header);
+}
+
+std::map<std::string, std::map<std::string, std::string>> getData(std::string fileName, std::vector<std::string> header)
+{
+
+    int pos = 0;
+    std::string line = "";
+    std::ifstream file(fileName);
+    std::map<std::string, std::map<std::string, std::string>> map;
+
+    getline(file, line); //
+    while (!file.eof())
+    {
+        std::map<std::string, std::string> tmp_map;
+
+        getline(file, line);
+        
+        pos = line.find(',');
+        std::string tmp_str = line.substr(0, pos);     
+        line = line.erase(0, pos + 1);
+
+        pos = line.find(',');
+        int count = 0;
+        while(pos != -1)
+        {
+            int tmp = line.find(',');
+            std::string data = line.substr(0, tmp);
+
+            tmp_map.insert(std::pair<std::string, std::string>(header[count], data));
+            
+            line = line.erase(0, tmp + 1);
+            pos = tmp;
+            count++;
+        }
+        
+        map.insert(std::pair<std::string, std::map<std::string, std::string>>(tmp_str, tmp_map));
+    }
+
+/*
+    for(std::pair<std::string, std::map<std::string, std::string>> item : map)
+    {
+        std::cout << item.first << " ";
+        for (std::pair<std::string, std::string> items :  item.second)
+        {
+            std::cout << items.second << " ";
+        }
+        std::cout << std::endl;
+    }
+*/
+    file.close();
+    return (map);
 }
--- parser.h
+++ parser.h
@@ -8,6 +8,7 @@
 #include <string>
 
 std::vector<std::string> getHeader(std::string fileName);
+std::map<std::string, std::map<std::string, std::string>> getData(std::string fileName, std::vector<std::string> header);
 
 void reader(std::string fileName);
 void output();
