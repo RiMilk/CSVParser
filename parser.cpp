b0a8bec0645146954143cb91dd0c9d2870aa05f9
RiMilk <rpakreeva@yandex.ru>
2023-01-30 01:17:49 +0300

    add arithmetic operations with data and output data

--- parser.cpp
+++ parser.cpp
@@ -4,15 +4,45 @@ void reader(std::string fileName)
 {
     std::ifstream file(fileName);
     std::vector<std::string> header;
-    std::map<std::string, std::map<std::string, std::string>> map;
+    std::vector<std::string> columns;
+    std::map<std::string, std::string> map;
 
     header = getHeader(fileName);
+    columns = getColumns(fileName);
     map = getData(fileName, header);
+    map = getCalc(map);
+    
+    output(map, header, columns);
 }
 
-void output()
+void output(std::map<std::string, std::string> map, std::vector<std::string> header, std::vector<std::string> columns)
 {
+    int iter = 0;
+    size_t count = 1;
 
+    for(std::string item : header)
+        std::cout << "," << item;
+
+    std::cout << std::endl;
+
+    for(std::pair<std::string, std::string> item : map)
+    {
+        if (count == 1)
+        {
+            std::cout << columns[iter];
+            iter++;
+        }
+
+        std::cout << " " << item.second;
+
+        if (count == header.size())
+        {
+            std::cout << std::endl;
+            count = 1;
+        }
+        else
+            count++;
+    }
 }
 
 std::vector<std::string> getHeader(std::string fileName)
@@ -43,13 +73,37 @@ std::vector<std::string> getHeader(std::string fileName)
     return (header);
 }
 
-std::map<std::string, std::map<std::string, std::string>> getData(std::string fileName, std::vector<std::string> header)
+std::vector<std::string> getColumns(std::string fileName)
 {
 
     int pos = 0;
     std::string line = "";
     std::ifstream file(fileName);
-    std::map<std::string, std::map<std::string, std::string>> map;
+    std::vector<std::string> columns;
+
+    getline(file, line); //
+    while (!file.eof())
+    {
+        std::map<std::string, std::string> tmp_map;
+
+        getline(file, line);
+        
+        pos = line.find(',');
+        std::string tmp_str = line.substr(0, pos);
+        columns.push_back(tmp_str);
+    }
+
+    file.close();
+    return (columns);
+}
+
+std::map<std::string, std::string> getData(std::string fileName, std::vector<std::string> header)
+{
+
+    int pos = 0;
+    std::string line = "";
+    std::ifstream file(fileName);
+    std::map<std::string, std::string> map;
 
     getline(file, line); //
     while (!file.eof())
@@ -69,27 +123,79 @@ std::map<std::string, std::map<std::string, std::string>> getData(std::string fi
             int tmp = line.find(',');
             std::string data = line.substr(0, tmp);
 
-            tmp_map.insert(std::pair<std::string, std::string>(header[count], data));
+            map.insert(std::pair<std::string, std::string>(header[count]+tmp_str, data));
             
             line = line.erase(0, tmp + 1);
             pos = tmp;
             count++;
         }
         
-        map.insert(std::pair<std::string, std::map<std::string, std::string>>(tmp_str, tmp_map));
     }
 
-/*
-    for(std::pair<std::string, std::map<std::string, std::string>> item : map)
-    {
-        std::cout << item.first << " ";
-        for (std::pair<std::string, std::string> items :  item.second)
-        {
-            std::cout << items.second << " ";
-        }
-        std::cout << std::endl;
-    }
-*/
     file.close();
+    return (map);
+}
+
+
+std::map<std::string, std::string> getCalc(std::map<std::string, std::string> map)
+{
+    for(std::pair<std::string, std::string> item : map)
+    {
+        if (item.second[0] == '=')
+        {
+            std::string tmp = item.second;
+
+            int posSum = tmp.find('+');
+            int posDiff = tmp.find('-');
+            int posMult = tmp.find('*');
+            int posDiv = tmp.find('/');
+
+            if (posSum != -1)
+            {
+                std::string left = tmp.substr(1, posSum - 1);
+                std::string right = tmp.substr(posSum + 1, tmp.length() - 1 - posSum);
+
+                auto ileft = map.find(left);
+                auto iright = map.find(right);
+
+                map[item.first] = std::to_string(std::atoi(ileft->second.c_str()) + std::atoi(iright->second.c_str()));
+            }
+
+            if (posDiff != -1)
+            {
+                std::string left = tmp.substr(1, posSum - 1);
+                std::string right = tmp.substr(posSum + 1, tmp.length() - 1 - posSum);
+
+                auto ileft = map.find(left);
+                auto iright = map.find(right);
+
+                map[item.first] = std::to_string(std::atoi(ileft->second.c_str()) - std::atoi(iright->second.c_str()));
+            }
+
+            if (posMult != -1)
+            {
+                std::string left = tmp.substr(1, posSum - 1);
+                std::string right = tmp.substr(posSum + 1, tmp.length() - 1 - posSum);
+
+                auto ileft = map.find(left);
+                auto iright = map.find(right);
+
+                map[item.first] = std::to_string(std::atoi(ileft->second.c_str()) * std::atoi(iright->second.c_str()));
+            }
+
+            if (posDiv != -1)
+            {
+                std::string left = tmp.substr(1, posSum - 1);
+                std::string right = tmp.substr(posSum + 1, tmp.length() - 1 - posSum);
+
+                auto ileft = map.find(left);
+                auto iright = map.find(right);
+
+                map[item.first] = std::to_string(std::atoi(ileft->second.c_str()) / std::atoi(iright->second.c_str()));
+            }
+            
+        }
+    }
+
     return (map);
 }
--- parser.h
+++ parser.h
@@ -8,9 +8,11 @@
 #include <string>
 
 std::vector<std::string> getHeader(std::string fileName);
-std::map<std::string, std::map<std::string, std::string>> getData(std::string fileName, std::vector<std::string> header);
+std::vector<std::string> getColumns(std::string fileName);
+std::map<std::string, std::string> getData(std::string fileName, std::vector<std::string> header);
+std::map<std::string, std::string> getCalc(std::map<std::string, std::string> map);
 
 void reader(std::string fileName);
-void output();
+void output(std::map<std::string, std::string> map, std::vector<std::string> header, std::vector<std::string> columns);
 
 #endif //PARSER_H
